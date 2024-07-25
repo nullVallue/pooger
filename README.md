87cd7b725e32d41a99942d1fae051ec3babe9ae2
Wei Jian <weijian.tan@whatifsolutions.my>
2024-07-25 16:46:29 +0800

    Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # pooger
 A website to log where you have poo'd
+
+Node Version : 18.20.2 lts
+Next.js Version : 14.2.5
---
+++ pooger/pages/pages/public/landingPage/index.js
@@ -0,0 +1,12 @@
+
+
+export default function LandingPage(){
+
+    return(
+        <>
+        <h1>Hello Welcome to Poogers</h1>
+        </>
+    );
+
+
+}
